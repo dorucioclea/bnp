32631453c7681c3ef226b824a1ffd84a6111aa9f
CronJ <suresh.varman@cronj.com>
2017-04-27 21:28:45 +0530

    url changed to /user instead /interaction

--- src/client/service/OnboardingUserService.js
+++ src/client/service/OnboardingUserService.js
@@ -3,7 +3,7 @@ import ajaxRequest from 'superagent';
 export default class OnboardingUserService {
   getOnboardingUserData(user) {
     return ajaxRequest.
-      get(`/interaction/api/onboardData/${user}`).
+      get(`/user/onboardData/${user}`).
       then(res => res.body).
       catch(err => ({
         status: err.status,
