51eaa71c98af04dc14452f20147bab0482676517
Lekealem Asong <lekealemasong@yahoo.de>
2017-06-30 11:08:23 +0200

    Add translation

--- src/client/components/SupplierRegistrationForm/i18n/de.js
+++ src/client/components/SupplierRegistrationForm/i18n/de.js
@@ -1,53 +1,6 @@
-let ApplicationFormConfirmation = {};
-ApplicationFormConfirmation.unsavedChanges = 'Sie haben Ihre Einträge nicht gespeichert. ' +
-  'Wenn Sie fortfahren, gehen diese verloren. Wollen Sie ohne zu speichern fortfahren?';
-
-let ApplicationFormTab = {};
-ApplicationFormTab.company = 'Unternehmen';
-ApplicationFormTab.address = 'Adressen';
-ApplicationFormTab.contact = 'Ansprechpartner';
-ApplicationFormTab.bankAccount = 'Bankkonten';
-ApplicationFormTab.financials = 'Finanzdaten';
-ApplicationFormTab.certification = 'Nachweise';
-ApplicationFormTab.classificationGroup = 'Warengruppen';
-ApplicationFormTab.logistics = 'Logistik';
-ApplicationFormTab.misc = 'Weiteres';
-ApplicationFormTab.rating = 'Bewertung';
-ApplicationFormTab.catalogsAndContracts = 'Kontrakte';
-
-let ApplicationFormButton = {};
-ApplicationFormButton.return = 'Nachbesserung anfordern';
-ApplicationFormButton.sendToCustomer = 'Bearbeitung abschlie\u00dfen und um Freigabe bitten';
-ApplicationFormButton.sendToCustomerConfirmation = 'Wollen Sie die Bearbeitung Ihrer Daten wirklich ' +
-  'abschlie\u00dfen? Sobald Sie die Daten zur Freigabe senden, ist keine weitere Bearbeitung mehr m\u00f6glich!';
-ApplicationFormButton.backToList = 'Zur\u00fcck zur \u00dcbersicht';
-
-let ApplicationFormLabel = {
-  rejectionReason: {}
-};
-ApplicationFormLabel.rejectionReason.label = 'Bitte geben Sie hier die Gr\u00FCnde an, ' +
-  'weshalb Sie die Lieferantendaten nicht genehmigen';
-ApplicationFormLabel.ok = 'OK';
-ApplicationFormLabel.icotermsDescription = 'Beschreibung der Incoterms';
-ApplicationFormLabel.graphic = 'Grafik';
-ApplicationFormLabel.successSendToCustomer = "Sie haben Ihre Unternehmensdaten " +
-  "erfolgreich bearbeitet. Sobald die Freigabe erfolgt ist, werden Sie per E-Mail darüber informiert.";
-
-let ApplicationFormHeader = {};
-ApplicationFormHeader.rejectionReason = 'Grund der Ablehnung';
-
-let ApplicationFormLink = {};
-ApplicationFormLink.header = 'In diesem Abschnitt können Sie die ' +
-  'allgemeinen Lieferbedingungen für unsere Läger herunterladen.';
-ApplicationFormLink.ladenbau = 'Allgemeine Lieferbedingungen Ladenbau';
-ApplicationFormLink.regional = 'Allgemeine Lieferbedingungen Regional- und Zentralläger';
-ApplicationFormLink.graphic = 'Diese ${separation} verdeutlicht die Bedeutung der Incoterms.';
+let RegistrationForm = {};
+RegistrationForm.supplierExists = 'Lieferant bereits vorhanden!';
 
 export default {
-  ApplicationFormConfirmation: ApplicationFormConfirmation,
-  ApplicationFormTab: ApplicationFormTab,
-  ApplicationFormButton: ApplicationFormButton,
-  ApplicationFormLabel: ApplicationFormLabel,
-  ApplicationFormHeader: ApplicationFormHeader,
-  ApplicationFormLink: ApplicationFormLink
+  RegistrationForm: RegistrationForm
 }
