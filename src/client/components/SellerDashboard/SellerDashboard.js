c44c5dde1602d20276655734aee5f6f446b25549
Lekealem Asong <lekealemasong@yahoo.de>
2017-09-28 11:37:03 +0200

    Remove react-bootstrap

--- src/client/components/SellerDashboard/SellerDashboard.js
+++ src/client/components/SellerDashboard/SellerDashboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import locales from './i18n/locales.js';
 import request from 'superagent-bluebird-promise';
-import { Col, Row, Image, Button } from 'react-bootstrap';
 import connect from 'react-redux/lib/components/connect';
 import serviceComponent from '@opuscapita/react-loaders/lib/serviceComponent';
 
@@ -63,7 +62,7 @@ class SellerDashboard extends React.Component {
   connectButton() {
     if (this.state.connectStatus === 'Connected') return '';
 
-    return (<Button bsStyle="warning" onClick={this.handleConnectionClick}>{this.state.i18n.getMessage('SellerDashboard.connections.connect')}</Button>)
+    return (<button className="btn btn-warning" onClick={this.handleConnectionClick}>{this.state.i18n.getMessage('SellerDashboard.connections.connect')}</button>)
   }
 
   connectionStatus() {
@@ -87,59 +86,61 @@ class SellerDashboard extends React.Component {
     return (
       <div>
         <br/>
-        <Row>
-          <Col md={6}>
+        <div className="row">
+          <div className="col-md-6">
             <div className="panel panel-success">
               <div className="panel-heading">
                 <h4>{this.state.i18n.getMessage('SellerDashboard.profileStrength.heading')}</h4>
               </div>
               <div className="panel-body">
-                <Col xs={6}>
-                  <SupplierProfileStrength
-                    key='profile_strength'
-                    actionUrl={this.context.simPublicUrl}
-                    supplierId={this.props.currentUserData.supplierid}
-                  />
-                </Col>
-                <Col xs={6}>
-                  <p>{this.state.i18n.getMessage('SellerDashboard.profileStrength.content')}</p>
-                  <Button bsStyle="warning" onClick={this.handleProfileClick}>
-                    {this.state.i18n.getMessage('SellerDashboard.profileStrength.editButton')}
-                  </Button>
-              </Col>
+                <div className="row">
+                  <div className="col-xs-6">
+                    <SupplierProfileStrength
+                      key='profile_strength'
+                      actionUrl={this.context.simPublicUrl}
+                      supplierId={this.props.currentUserData.supplierid}
+                    />
+                  </div>
+                  <div className="col-xs-6">
+                    <p>{this.state.i18n.getMessage('SellerDashboard.profileStrength.content')}</p>
+                    <button className="btn btn-warning" onClick={this.handleProfileClick}>
+                      {this.state.i18n.getMessage('SellerDashboard.profileStrength.editButton')}
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </Col>
-          <Col md={6}>
+          </div>
+          <div className="col-md-6">
             <div className="panel panel-success">
               <div className="panel-heading">
                 <h4>{this.state.i18n.getMessage('SellerDashboard.connections.heading')}</h4>
               </div>
               <div className="panel-body">
-                <Row>
-                  <Col xs={4}>
+                <div className="row">
+                  <div className="col-xs-4">
                     <span className="fa-stack fa-lg">
                       <i className="fa fa-circle fa-stack-2x" style={this.colorStyle()}></i>
                       <i className="fa fa-power-off fa-stack-1x fa-inverse"></i>
                     </span>
-                  </Col>
-                  <Col xs={4}>
+                  </div>
+                  <div className="col-xs-4">
                     <h4>eInvoice</h4>
                     <i style={this.colorStyle()}>{this.connectionStatus()}</i>
-                  </Col>
-                  <Col xs={4}>{this.connectButton()}</Col>
-                </Row>
-                <Row>
-                  <Col xs={12}>
+                  </div>
+                  <div className="col-xs-4">{this.connectButton()}</div>
+                </div>
+                <div className="row">
+                  <div className="col-xs-12">
                     <br/>
                     <p>{this.state.i18n.getMessage('SellerDashboard.connections.content')}</p>
                     <br/>
-                  </Col>
-                </Row>
+                  </div>
+                </div>
               </div>
             </div>
-          </Col>
-        </Row>
+          </div>
+        </div>
       </div>
     )
   }
