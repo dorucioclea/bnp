be07eb6e1897008559c63967d1f7d905f9562ea7
Lekealem Asong <lekealemasong@yahoo.de>
2017-10-12 14:21:56 +0200

    Embed supplier directory

--- src/client/components/SupplierDirectory/SupplierDirectory.js
+++ src/client/components/SupplierDirectory/SupplierDirectory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import locales from './i18n/locales.js';
+import serviceComponent from '@opuscapita/react-loaders/lib/serviceComponent';
 
 export default class SupplierDirectory extends React.Component {
   static contextTypes = {
@@ -7,6 +8,15 @@ export default class SupplierDirectory extends React.Component {
   };
 
   componentWillMount() {
+    let serviceRegistry = (service) => ({ url: `/supplier` });
+    const Directory = serviceComponent({
+      serviceRegistry,
+      serviceName: 'supplier' ,
+      moduleName: 'supplier-directory',
+      jsFileName: 'directory-bundle'
+    });
+
+    this.externalComponents = { Directory };
     this.setState({ i18n: this.context.i18n.register('SupplierDirectory', locales) });
   }
 
@@ -15,9 +25,11 @@ export default class SupplierDirectory extends React.Component {
   }
 
   render() {
+    const { Directory } = this.externalComponents;
     return (
       <div>
         <h1>{this.state.i18n.getMessage('SupplierDirectory.head')}</h1>
+        <Directory actionUrl={this.context.simPublicUrl} />
       </div>
     )
   }
