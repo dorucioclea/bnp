53185cab7803c92c2033a09607160b792c567df6
Lekealem Asong <lekealemasong@yahoo.de>
2018-11-21 12:45:59 +0100

    Add guidelines doc to create customer page

--- src/client/containers/Admin/CustomerList.js
+++ src/client/containers/Admin/CustomerList.js
@@ -33,11 +33,6 @@ export default class CustomerList extends Components.ContextComponent {
     return (
       <div>
         <button className='btn btn-primary pull-right' onClick={this.handleCreateCustomerClick.bind(this)} >{this.context.i18n.getMessage('CustomerList.createCustomer')}</button>
-        <a
-          target="_blank"
-          href="https://docs.google.com/document/d/1XHO3D2_10iTBmtZhhbStJ7oD4uZiTJoedjmTiH9I8qg/edit#heading=h.gjdgxs"
-          className="btn btn-link pull-right"
-        >{this.context.i18n.getMessage('CustomerList.createGuidelines')}</a>
         <this.CustomerList
           onEdit={customerId => this.handleEdit(customerId)}
           onCreateUser={customerId => this.handleCreateUser(customerId)}
