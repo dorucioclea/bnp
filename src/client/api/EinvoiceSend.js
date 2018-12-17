d6dfb8eda0513a07eaca2abf7f50c3dd7d14186a
Lekealem Asong <lekealemasong@yahoo.de>
2018-12-17 14:58:15 +0100

    Get config data from einvoice send

--- src/client/api/EinvoiceSend.js
+++ src/client/api/EinvoiceSend.js
@@ -5,6 +5,10 @@ class EinvoiceSend extends ApiBase {
     return this.ajax.get(`/einvoice-send/api/config/inchannels/${supplierId}`).
     set('Accept', 'application/json').then(response => response.body);
   }
+
+  all(supplierIds) {
+    return Promise.all(supplierIds.map(supplierId => this.get(supplierId).catch(() => null)));
+  }
 }
 
 export default EinvoiceSend;
