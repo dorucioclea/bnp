664aca2905ea1bde21d89a5395d6e288f13fa910
Lekealem Asong <lekealemasong@yahoo.de>
2018-12-19 10:44:23 +0100

    Delete method not needed anymore

--- src/client/api/EinvoiceSend.js
+++ src/client/api/EinvoiceSend.js
@@ -1,11 +1,6 @@
 import ApiBase from './ApiBase';
 
 class EinvoiceSend extends ApiBase {
-  get(supplierId) {
-    return this.ajax.get(`/einvoice-send/api/config/inchannels/${supplierId}`).
-    set('Accept', 'application/json').then(response => response.body);
-  }
-
   all(supplierIds) {
     const queryParams = supplierIds.length > 0 ? { id: supplierIds.join(',') } : {};
     return this.ajax.get(`/einvoice-send/api/config/inchannels`).set('Accept', 'application/json').
