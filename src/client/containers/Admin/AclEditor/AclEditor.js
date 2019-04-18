83aec0678e5904cc322e01e542c59ac4467bc34a
Christian Drengenberg <Christian.Drengenberg@opuscapita.com>
2019-04-18 14:00:13 +0200

    Changed AclEditor to new UI.

--- src/client/containers/Admin/AclEditor/AclEditor.js
+++ src/client/containers/Admin/AclEditor/AclEditor.js
@@ -3,35 +3,40 @@ import { Components } from '@opuscapita/service-base-ui';
 import translations from '../../i18n/index'
 import './AclEditor.css';
 
-export default class AclEditor extends Components.ContextComponent {
-
-    constructor(props, context) {
+export default class AclEditor extends Components.ContextComponent
+{
+    constructor(props, context)
+    {
         super(props);
 
         context.i18n.register('AclEditor', translations);
 
-        this.AclEditor = context.loadComponent({
-            serviceName: 'acl' ,
-            moduleName: 'acl-editor',
-            jsFileName: 'editor-bundle'
+        this.RoleManager = context.loadComponent({
+            serviceName: 'user',
+            moduleName: 'user-role-manager',
+            jsFileName: 'role-manager-bundle'
         });
 
-        this.UserRolePicker = context.loadComponent({
-            serviceName: 'user' ,
-            moduleName: 'user-rolepicker',
-            jsFileName: 'rolepicker-bundle'
-        });
+        // this.AclEditor = context.loadComponent({
+        //     serviceName: 'acl' ,
+        //     moduleName: 'acl-editor',
+        //     jsFileName: 'editor-bundle'
+        // });
+
+        // this.UserRolePicker = context.loadComponent({
+        //     serviceName: 'user' ,
+        //     moduleName: 'user-rolepicker',
+        //     jsFileName: 'rolepicker-bundle'
+        // });
     }
 
-    /**
-     * Role changed handler
-     * @param {string} roleId Role identifier
-     */
-    roleChange(roleId) {
-        this.props.router.push(`/bnp/permissions/${roleId}`);
-    }
+    // roleChange(roleId)
+    // {
+    //     this.props.router.push(`/bnp/permissions/${roleId}`);
+    // }
 
-    render() {
+    render()
+    {
         const i18n = this.context.i18n.getMessage.bind(this.context.i18n);
         this.context.setPageTitle(i18n('AclEditor.page.title'));
 
@@ -39,17 +44,18 @@ export default class AclEditor extends Components.ContextComponent {
             <div>
                 <section>
                     <h2>{i18n('AclEditor.authority.heading')}</h2>
-                    <this.UserRolePicker
+                    {/* <this.UserRolePicker
                         className="acl-admin-role-picker"
                         value={this.props.params.roleId}
                         onChange={roleId => this.roleChange(roleId)}
-                    />
+                    /> */}
                 </section>
 
                 {this.props.params.roleId &&
                     <section>
-                        <h2>{i18n('AclEditor.permissions.heading')}</h2>
-                        <this.AclEditor role={this.props.params.roleId} />
+                        {/* <h2>{i18n('AclEditor.permissions.heading')}</h2>
+                        <this.AclEditor role={this.props.params.roleId} /> */}
+                        <this.RoleManager />
                     </section>}
             </div>
         )
